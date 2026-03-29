57662cef8e39ede3e00f014dac4e1239b4172fa6
Elian Melo <elianmelomorais@gmail.com>
2026-03-29 15:05:00 -0300

    Implemented all magic vfx

--- Assets/_ProjectAPhysicsMage/Scripts/PlayerMagicController.cs
+++ Assets/_ProjectAPhysicsMage/Scripts/PlayerMagicController.cs
@@ -28,15 +28,7 @@ public class PlayerMagicController : MonoBehaviour
     private MagicElement _element;
     private MagicDirection _direction;
 
-    [SerializeField] private GameObject _earthForward;
-    [SerializeField] private GameObject _earthAround;
-    [SerializeField] private GameObject _earthShield;
-    [SerializeField] private GameObject _waterForward;
-    [SerializeField] private GameObject _waterAround;
-    [SerializeField] private GameObject _waterShield;
-    [SerializeField] private GameObject _windForward;
-    [SerializeField] private GameObject _windAround;
-    [SerializeField] private GameObject _windShield;
+    [SerializeField] private VFXController vfxController;
 
     void Update()
     {
@@ -98,31 +90,7 @@ public class PlayerMagicController : MonoBehaviour
 
     private void CastMagic()
     {
-        GameObject prefab = CalculatedPrefabBasedOnData();
+        vfxController.CastMagicVFX(_element, _direction);
         ChangeToElementPhase();
-    }
-
-    private GameObject CalculatedPrefabBasedOnData()
-    {
-        if (_element == MagicElement.Earth && _direction == MagicDirection.Forward)
-            return _earthForward;
-        if (_element == MagicElement.Earth && _direction == MagicDirection.Around)
-            return _earthAround;
-        if (_element == MagicElement.Earth && _direction == MagicDirection.Shield)
-            return _earthShield;
-        if (_element == MagicElement.Water && _direction == MagicDirection.Forward)
-            return _waterForward;
-        if (_element == MagicElement.Water && _direction == MagicDirection.Around)
-            return _waterAround;
-        if (_element == MagicElement.Water && _direction == MagicDirection.Shield)
-            return _waterShield;
-        if (_element == MagicElement.Wind && _direction == MagicDirection.Forward)
-            return _windForward;
-        if (_element == MagicElement.Wind && _direction == MagicDirection.Around)
-            return _windAround;
-        if (_element == MagicElement.Wind && _direction == MagicDirection.Shield)
-            return _windShield;
-        Debug.LogError("Unexpected error!");
-        return _earthForward;
     }
 }
--- Assets/_ProjectAPhysicsMage/Scripts/VFX/VFXController.cs
+++ Assets/_ProjectAPhysicsMage/Scripts/VFX/VFXController.cs
@@ -1,6 +1,6 @@
+using AYellowpaper.SerializedCollections;
 using PixPlays.ElementalVFX;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class VFXController : MonoBehaviour
@@ -8,19 +8,34 @@ public class VFXController : MonoBehaviour
     [System.Serializable]
     public class VFXData
     {
-        public string Name;
-        public AnimationClip clip;
         public float VfxSpawnDelay;
         public BindingPointType Source;
         public float _Duration;
         public float _Radius;
         public bool _FollowPlayer;
+        public bool _Reverse;
         public BaseVfx VFX;
     }
 
-    [SerializeField] List<VFXData> _Data;
+    [System.Serializable]
+    public struct MagicVFX
+    {
+        public MagicElement element;
+        public MagicDirection direction;
+
+        public MagicVFX(MagicElement element, MagicDirection direction)
+        {
+            this.element = element;
+            this.direction = direction;
+        }
+    }
+
+    [SerializedDictionary("MagicVFX", "VFXData")]
+    public SerializedDictionary<MagicVFX, VFXData> _magicVFXToVFXData;
+
     [SerializeField] VFXCharacter _VFXCharacter;
-    [SerializeField] string _CurrentData;
+    private MagicVFX _currentMagicVFX;
+    private VFXData _currentVFXData;
 
     private int index = 0;
 
@@ -28,19 +43,31 @@ public class VFXController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.U))
         {
-        StartCoroutine(Coroutine_Spawn());    
+            CastMagicVFX(MagicElement.Wind, MagicDirection.Shield);
         }
     }
 
-    IEnumerator Coroutine_Spawn()
+    public void CastMagicVFX(MagicElement element, MagicDirection direction)
     {
-        _VFXCharacter.PlayAnimation("New Animation", _Data[index].clip);
-        yield return new WaitForSeconds(_Data[index].VfxSpawnDelay);
-        BaseVfx go = Instantiate(_Data[index].VFX);
-        if(_Data[index]._FollowPlayer)
+        _currentMagicVFX = new MagicVFX(element, direction);
+        GetDataByCurrentMagic();
+        StartCoroutine(SpawnVFX());
+    }
+
+    private void GetDataByCurrentMagic()
+    {
+        if (!_magicVFXToVFXData.ContainsKey(_currentMagicVFX)) return;
+        _currentVFXData = _magicVFXToVFXData[_currentMagicVFX];
+    }
+
+    IEnumerator SpawnVFX()
+    {
+        yield return new WaitForSeconds(_currentVFXData.VfxSpawnDelay);
+        BaseVfx go = Instantiate(_currentVFXData.VFX);
+        if(_currentVFXData._FollowPlayer)
             go.transform.SetParent(transform);
-        Transform sourcePoint = _VFXCharacter.BindingPoints.GetBindingPoint(_Data[index].Source);
-        var vfxData = new VfxData(sourcePoint, _VFXCharacter.GetTarget(), _Data[index]._Duration, _Data[index]._Radius);
+        Transform sourcePoint = _VFXCharacter.BindingPoints.GetBindingPoint(_currentVFXData.Source);
+        var vfxData = new VfxData(sourcePoint, _VFXCharacter.GetTarget(), _currentVFXData._Duration, _currentVFXData._Radius);
         vfxData.SetGround(_VFXCharacter.BindingPoints.GetBindingPoint(BindingPointType.Ground));
         go.Play(vfxData);
     }
